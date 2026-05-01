d7f7f8473c24a9c9cdb141fffa6c86162ad6c784
Efekan Dagkiran <dagkiranefekan@gmail.com>
2026-05-01 14:35:38 +0200

    Add project files.

---
+++ OOPLinkedListsProject/GameField.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLinkedListsProject
+{
+    public class GameField
+    {
+        private FieldNode? head = null;
+        private FieldNode? tail = null;
+
+        public GameField(Player player1, Player player2)
+        {
+            int length;
+
+            do
+            {
+                Console.WriteLine("How Long should the Map be? (10 <= input <= 500) ");
+                length = Convert.ToInt32(Console.ReadLine());
+
+            } while (length < 10 && length > 500);
+
+
+
+            FieldNode firstNode = new FieldNode();
+
+            head = firstNode;
+            tail = firstNode;
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                FieldNode newNode = new FieldNode();
+                FillNodeWithInfo(newNode, i);
+                head.prev = newNode;
+                newNode.next = head;
+                head = newNode;
+                newNode.prev = null;
+            }
+
+            player1.position = player2.position = head;
+        }
+
+
+
+        private void FillNodeWithInfo(FieldNode node, int pos)
+        {
+            node.tileNr = pos;
+
+            if(pos % 5 == 0)
+            {
+                node.Ladder = true;
+            }
+
+            else if (pos % 8 == 0)
+            {
+                node.Snake = true; 
+            }
+        }
+
+        void FixTileNr(Player currentPlayer, int addedTiles)
+        {
+            int startTileNr = currentPlayer.position.tileNr;
+            int tmp = startTileNr;
+
+            for(int i = 0; i < addedTiles; i++)
+            {
+                currentPlayer.BackwardMove(this, 1);
+                currentPlayer.position.tileNr = --tmp;
+            }
+
+            while (currentPlayer.position != head) // should be the head at the end
+            {
+                currentPlayer.BackwardMove(this, 1);
+                currentPlayer.position.tileNr -= 5;
+            }
+
+            currentPlayer.ForewardMove(this, startTileNr - 1);
+
+            
+
+
+
+
+            
+        }
+        
+
+
+
+        public void PrintCurrentState(int round, Player p1, Player p2)
+        {
+            Console.Clear();
+            Console.WriteLine($"Current Round: {round}");
+
+            Console.WriteLine($"{p1.name} is currently in Tile: {p1.position.tileNr}"); //TODO IF APPENDING NEW NODES WATHC OUT THAT TILENR IS STILL ACCURATE
+            Console.WriteLine($"{p2.name} is currently in Tile: {p2.position.tileNr}");
+
+        }
+
+
+        public void Update(Player currentPlayer,Player otherPlayer,  int diceThrow )
+        {
+            currentPlayer.ForewardMove(this ,diceThrow);
+
+            if (currentPlayer.position == tail) //win takes precendence over snake and other stuff. 
+            {
+                currentPlayer.won = true;
+                return;
+            }
+
+
+            if (currentPlayer.position.Snake)
+            {
+                currentPlayer.BackwardMove(this, 3);
+                Console.WriteLine("SNAKE!!");
+
+            }
+
+            else if (currentPlayer.position.Ladder)
+            {
+                currentPlayer.ForewardMove(this, 3);
+                Console.WriteLine("LADDERR!!!");
+
+            }
+
+
+            if (currentPlayer.position == otherPlayer.position)
+            {
+                currentPlayer.BackwardMove(this, 1);
+                Console.WriteLine("YOU HIT THE OTHER PLAYER!!");
+
+            }
+
+            if (diceThrow == 1)
+            {
+                ExpandMapEnd(5);
+                Console.WriteLine("The Map expands by 5 tiles!");
+
+            }
+
+            if (diceThrow == 6)
+            {
+                int n = 5;
+                Console.WriteLine("You expand the map behind u by 5 tiles!");
+                ExpandMapBeforeCurrent(n, currentPlayer);                
+                //FixTileNr(currentPlayer, n);
+
+            }
+
+        }
+
+       
+
+        private void ExpandMapEnd(int n)
+        {
+            FieldNode newNode = new FieldNode();
+            for (int i = 0; i < n; i++)
+            {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+                newNode.next = null;
+            }
+        }
+        private void ExpandMapBeforeCurrent(int n, Player currentPlayer) //before means left of it
+        {
+            //cant be tail, because checks in Update() for it:
+            
+            
+            if(currentPlayer.position == head)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    FieldNode newNode = new FieldNode();
+                    head.prev = newNode;
+                    newNode.next = head;
+                    head = newNode;
+                    newNode.prev = null;
+                }
+                
+            }
+            else
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    FieldNode newNode = new FieldNode();
+                    newNode.next = currentPlayer.position;
+                    currentPlayer.position.prev.next = newNode;
+                    newNode.prev = currentPlayer.position.prev;
+                    currentPlayer.position.prev = newNode;
+
+                }
+            }
+
+
+
+
+        }
+
+
+        public class FieldNode
+        {
+            public bool Snake { get; set; } = false;
+            public bool Ladder { get; set; } = false;
+
+            public FieldNode? next = null;
+            public FieldNode? prev = null;
+            public int tileNr;
+        }
+
+
+        public class Player
+        {
+            public bool won;
+            public string name;
+            public FieldNode position;
+
+            public Player(string name)
+            {
+                this.name = name;
+            }
+
+
+            public void ForewardMove(GameField game, int n)
+            {
+                while (position!= game.tail)
+                {
+                    if (n-- == 0) break;
+
+                    position = position.next;
+
+                }
+            }
+
+            public void BackwardMove(GameField game, int n)
+            {
+                while (position != game.head)
+                {
+                    if (n-- == 0) break;
+
+                    position = position.prev;
+
+
+                }
+            }
+
+            public static int ThrowDice()
+            {
+                Console.WriteLine("Press enter to throw dice");
+                Console.ReadLine();
+                Random rd = new Random();
+                int diceThrow = rd.Next(6) + 1; 
+                return diceThrow;
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ OOPLinkedListsProject/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace OOPLinkedListsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameField.Player player1 = new GameField.Player("x");
+            GameField.Player player2 = new GameField.Player("y");
+            GameField game = new GameField(player1, player2);
+
+            int diceP1 = -1;
+            int diceP2 = -1;
+            int round = 1;
+
+
+            while (!(player1.won || player2.won))
+            {
+
+                game.PrintCurrentState(round++, player1 , player2);     //change player each loop iteration
+
+
+                    Console.WriteLine("Player One Throws his Dice: ");
+                    diceP1 = GameField.Player.ThrowDice();
+                    Console.WriteLine($"Player One got a {diceP1}");
+
+                    game.Update(player1, player2, diceP1); //need both players to check if same position. updates player 1
+                    if (player1.won) break;
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Player Two Throws his Dice: ");
+                    diceP2 = GameField.Player.ThrowDice();
+                    Console.WriteLine($"Player Two got a {diceP2}");
+
+                    game.Update(player2, player1, diceP2);
+
+                    Console.WriteLine("Ready for next Round?");
+                    Console.ReadLine();
+            }
+
+            if (player1.won)
+            {
+                Console.WriteLine($"Congratiulation {player1.name}!!! YOU WONNN");
+            }
+            else if (player2.won)
+            {
+                Console.WriteLine($"Congratiulation {player2.name}!!! YOU WONNN");
+            }
+
+        }
+    }
+}
